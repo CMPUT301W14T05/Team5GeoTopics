524b02705c3f148ef38e6c5413f8469ab9bb5fdb
slmyers <slmyers@ualberta.ca>
2014-02-08 11:14:44 -0700

    Removed auto generated code in GeoTopicsActivity.

--- src/main/GeoTopicsActivity.java
+++ src/main/GeoTopicsActivity.java
@@ -6,17 +6,5 @@ import android.view.Menu;
 
 public class GeoTopicsActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_geo_topics);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.geo_topics, menu);
-		return true;
-	}
 
 }
