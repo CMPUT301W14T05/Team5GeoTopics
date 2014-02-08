de5f39947861efde101f64f018e84e53f7c06d59
slmyers <slmyers@ualberta.ca>
2014-02-08 11:02:52 -0700

    changed JUnitTests to JUnit4, and fixed a "stray import".

--- src/main/GeoTopicsActivity.java
+++ src/main/GeoTopicsActivity.java
@@ -1,7 +1,22 @@
 package main;
 
+import android.os.Bundle;
 import android.app.Activity;
+import android.view.Menu;
 
 public class GeoTopicsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_geo_topics);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.geo_topics, menu);
+		return true;
+	}
 
 }
--- src/tests/UseCase20.java
+++ src/tests/UseCase20.java
@@ -1,33 +1,32 @@
 package tests;
 
-/* 	Use Case Name:	GetCommentsOnInternetReConnect
-  	User Story:		As a user, if my phone connects to the internet while this app is open it should
-  					load the latest and greatest and most relevant comments.
-   
-   	Goal: 			Upon connecting to the internet the app should load the freshest and most relevant comments.
-   	Trigger: 		Phone connects to the internet while the app is running.
-
-	Pre condition: 	App is running and not connected to the internet.
-
-	Post condition:	The latest and most relevant comments are retrieved.
-
-	Basic Flow:
-
-					User has the app open but not connected to the internet.
-					Phone connects to the internet.
-					Newest comments are retrieved from the server.
-	Exceptions:
-
-					3.) Phone loses internet before all comments are retrieved 
-					3.1) Abandon the comments retrieval and try again when we get internet.
-	
-	last updated: Feb 6/2014 4:48pm slmyers
- */
-
 import java.util.ArrayList;
 import main.GeoTopicsActivity;
 import android.test.ActivityInstrumentationTestCase2;
 
+/* 	Use Case Name:	GetCommentsOnInternetReConnect
+	User Story:		As a user, if my phone connects to the internet while this app is open it should
+					load the latest and greatest and most relevant comments.
+
+	Goal: 			Upon connecting to the internet the app should load the freshest and most relevant comments.
+	Trigger: 		Phone connects to the internet while the app is running.
+
+Pre condition: 	App is running and not connected to the internet.
+
+Post condition:	The latest and most relevant comments are retrieved.
+
+Basic Flow:
+
+				User has the app open but not connected to the internet.
+				Phone connects to the internet.
+				Newest comments are retrieved from the server.
+Exceptions:
+
+				3.) Phone loses internet before all comments are retrieved 
+				3.1) Abandon the comments retrieval and try again when we get internet.
+
+last updated: Feb 8/2014 11:00am slmyers
+*/
 public class UseCase20 extends ActivityInstrumentationTestCase2<GeoTopicsActivity> {
 
 	public UseCase20() {
@@ -45,12 +44,10 @@ public class UseCase20 extends ActivityInstrumentationTestCase2<GeoTopicsActivit
 		try{
 			wsc.getCommentsOnConnectivity(commentList);
 		}catch(Exception 3){ // reference to exception above
-			
+
 		}catch(Exception 3.1){ // reference to excetpion above
-			
+
 		}
 		assertTrue(commentList.contains(c));
 	}
-	
-	
 }
