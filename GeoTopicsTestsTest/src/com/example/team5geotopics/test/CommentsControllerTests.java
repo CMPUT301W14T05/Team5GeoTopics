7ba7dbb304c7aa305a30a41251c0cca840574d6a
twendlandt <twendlan@ualberta.ca>
2014-02-12 14:29:32 -0700

    added skeletion test case code/STILL NEEDS TEST CODE

---
+++ GeoTopicsTestsTest/src/com/example/team5geotopics/test/CommentsControllerTests.java
@@ -0,0 +1,73 @@
+package com.example.team5geotopics.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/* 
+ * contains the test code for all methods in the CommentsController class
+ * 
+ * NOTE: this is currently skeleton test code - please add to it
+ */
+public class CommentsControllerTests extends
+		ActivityInstrumentationTestCase2<CommentsController> {
+
+	public CommentsControllerTests() {
+		super(CommentsController.class);
+	}
+
+	/*
+	 * Use Case 5: MakeNewTopLevelComment Test
+	 */
+	public void MakeNewTopLevelCommentTest() {
+		
+	}
+	
+	/*
+	 * Use Case 6: BrowseTopLevelComments Test
+	 */
+	public void BrowseTopLevelCommentsTest() {
+		
+	}
+	
+	/*
+	 * Use Case 7: BrowseCommentReplies Test
+	 */
+	public void BrowseCommentRepliesTest() {
+		
+	}
+	
+	/*
+	 * Use Case 8: ReplyToComment Test
+	 */
+	public void ReplyToCommentTest() {
+		
+	}
+	
+	/*
+	 * Use Case 9: AddPictureToComment Test
+	 */
+	public void AddPictureToCommentTest() {
+		
+	}
+	
+	/*
+	 * Use Case 12: SaveComentAsFavoritees Test
+	 */
+	public void SaveCommentAsFavoritTest() {
+		
+	}
+
+	/*
+	 * Use Case 14: EditAuthoredComment Test
+	 */
+	public void EditAuthoredCommentTest() {
+		
+	}
+	
+	/*
+	 * Use Case 16: GetLatestComments Test
+	 */
+	public void GetLatestComments() {
+		
+	}
+	
+}
