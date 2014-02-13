8e17f5d5fc50db96ccc2ec1748a23eeeb5f61946
twendlandt <twendlan@ualberta.ca>
2014-02-12 20:50:09 -0700

    moved usedcase20 into web service controller

--- GeoTopicsTests/src/ca/ualberta/cs/team5geotopics/test/WebServiceControllerTest.java
+++ GeoTopicsTests/src/ca/ualberta/cs/team5geotopics/test/WebServiceControllerTest.java
@@ -69,5 +69,48 @@ public class WebServiceControllerTest extends
 		wsc.getCommentsOnConnectivity(cl);
 		assertTrue(cl.contains(c));
 	}
+	
+	
+	/* 	Use Case Name:	GetCommentsOnInternetReConnect
+		User Story:		As a user, if my phone connects to the internet while this app is open it should
+						load the latest and greatest and most relevant comments.
+	
+		Goal: 			Upon connecting to the internet the app should load the freshest and most relevant comments.
+		Trigger: 		Phone connects to the internet while the app is running.
+	
+		Pre condition: 	App is running and not connected to the internet.
+		
+		Post condition:	The latest and most relevant comments are retrieved.
+		
+		Basic Flow:
+		
+						User has the app open but not connected to the internet.
+						Phone connects to the internet.
+						Newest comments are retrieved from the server.
+		Exceptions:
+		
+						3.) Phone loses internet before all comments are retrieved 
+						3.1) Abandon the comments retrieval and try again when we get internet.
+		
+		updated: Feb 8/2014 11:00am slmyers
+		last update: Feb 12/2014 8:48pm twendlan
+	*/
+	public void GetCommentsOnInternetReconnectTest(){
+		WebServiceController wsc = new WebServiceController();
+		Comment c = new Comment("Test");
+		// ensure connectivity via sometype of code here
+		wsc.pushCommentOnConnectivity(c);
+		ArrayList<Comment> commentList = new ArrayList<Comment>();
+		// ensure that internet connectiviy is disabled via some type of code here
+		// then turn on internet connectivity
+		try{
+			wsc.getCommentsOnConnectivity(commentList);
+		}catch(Exception 3){ // reference to exception above
+
+		}catch(Exception 3.1){ // reference to excetpion above
+
+		}
+		assertTrue(commentList.contains(c));
+	}
 
 }
