1b168bcac6f8ffb06d8697d29c471d6978234d71
brycecartman <cartman@ualberta.ca>
2014-03-04 15:56:03 -0700

    Working on basic screen for StartActivity

--- GeoTopicsActivity/src/ca/ualberta/cs/team5geotopics/StartActivity.java
+++ GeoTopicsActivity/src/ca/ualberta/cs/team5geotopics/StartActivity.java
@@ -5,20 +5,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.team5geotopics.R;
 
 
 public class StartActivity extends Activity {
-	private Button mBrowseTopLevel;
+	//private Button mBrowseTopLevel;
+	private ImageButton mBrowseTopLevel;
+	private ImageButton mBrowseMyComments;
+	private ImageButton mBrowseFavourites;
+	private ImageButton mBrowseBookmarks;
 	private Button mTestEsBtn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_geo_topics);
 		
-		setUpBrowseButton();
+		
 		setUpEsTestButton();
+		setUpBrowseButton();
+		setUpFavouritesButton();
+		setUpMyCommentsButton();
+		setUpBrowseBookmarks();
 	}
 	
 	private void setUpEsTestButton() {
@@ -35,7 +44,7 @@ public class StartActivity extends Activity {
 	}
 
 	private void setUpBrowseButton() {
-		mBrowseTopLevel = (Button)findViewById(R.id.start_browse_top_level);
+		mBrowseTopLevel = (ImageButton)findViewById(R.id.start_browse_top_level);
 		mBrowseTopLevel.setOnClickListener(new View.OnClickListener() 
 		{
 			
@@ -50,4 +59,51 @@ public class StartActivity extends Activity {
 		});
 	}
 	
+	private void setUpMyCommentsButton() {
+		mBrowseTopLevel = (ImageButton)findViewById(R.id.start_my_comments);
+		mBrowseTopLevel.setOnClickListener(new View.OnClickListener() 
+		{
+			
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(StartActivity.this, BrowseActivity.class);
+				//This lets the browse activity know that we are viewing top level comments
+				GeoTopicsApplication.setCurrentViewingComment(null);
+				startActivity(myIntent);
+				
+			}
+		});
+	}
+	
+	private void setUpFavouritesButton() {
+		mBrowseTopLevel = (ImageButton)findViewById(R.id.start_favourite);
+		mBrowseTopLevel.setOnClickListener(new View.OnClickListener() 
+		{
+			
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(StartActivity.this, BrowseActivity.class);
+				//This lets the browse activity know that we are viewing top level comments
+				GeoTopicsApplication.setCurrentViewingComment(null);
+				startActivity(myIntent);
+				
+			}
+		});
+	}
+	
+	private void setUpBrowseBookmarks() {
+		mBrowseTopLevel = (ImageButton)findViewById(R.id.start_bookmark);
+		mBrowseTopLevel.setOnClickListener(new View.OnClickListener() 
+		{
+			
+			@Override
+			public void onClick(View v) {
+				Intent myIntent = new Intent(StartActivity.this, BrowseActivity.class);
+				//This lets the browse activity know that we are viewing top level comments
+				GeoTopicsApplication.setCurrentViewingComment(null);
+				startActivity(myIntent);
+				
+			}
+		});
+	}
 }
