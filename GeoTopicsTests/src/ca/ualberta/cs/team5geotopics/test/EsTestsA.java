196a4ae997677056cba947b26ba2ed03e566f2ce
slmyers <slmyers@ualberta.ca>
2014-03-13 18:47:35 -0600

    adding new tests

---
+++ GeoTopicsTests/src/ca/ualberta/cs/team5geotopics/test/EsTestsA.java
@@ -0,0 +1,58 @@
+package ca.ualberta.cs.team5geotopics.test;
+
+import io.searchbox.client.JestResult;
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+import ca.ualberta.cs.team5geotopics.BrowseActivity;
+import ca.ualberta.cs.team5geotopics.CommentListModel;
+import ca.ualberta.cs.team5geotopics.CommentSearch;
+import ca.ualberta.cs.team5geotopics.TopLevelActivity;
+
+
+public class EsTestsA extends ActivityInstrumentationTestCase2<TopLevelActivity> {
+	private Activity mActivity;
+	
+	public EsTestsA() {
+		super(TopLevelActivity.class);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		mActivity = getActivity();
+	}
+	
+	public void testPullTopLevel(){
+		CommentListModel listModel = new CommentListModel();
+		CommentSearch search = new CommentSearch(listModel);
+		
+		Thread thread = search.pullTopLevel((BrowseActivity) mActivity);
+		try{
+			thread.join();
+		} catch (InterruptedException e) {
+			Log.w("EsTestPush", "Thread interrupt");
+		}
+		
+		JestResult result = search.returnResult();
+		assertTrue("Result is not null", result != null);
+		assertTrue("Result is successful", result.isSucceeded());
+	}
+	
+	public void testPullReplies(){
+		CommentListModel listModel = new CommentListModel();
+		CommentSearch search = new CommentSearch(listModel);
+		
+		Thread thread = search.pullReplies((BrowseActivity) mActivity, "test id");
+		try{
+			thread.join();
+		} catch (InterruptedException e){
+			Log.w("EsTestPush", "Thread interrupt");
+		}
+		
+		JestResult result = search.returnResult();
+		assertTrue("Result is not null", result != null);
+		assertTrue("Result is successful", result.isSucceeded());
+	}
+}
