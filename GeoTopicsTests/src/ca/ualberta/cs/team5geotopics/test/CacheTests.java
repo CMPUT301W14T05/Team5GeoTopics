246cf13de04f1af67004d4322f9755828c101e85
MattDekinder <dekinder@ualberta.ca>
2014-03-15 19:34:25 -0600

    created test for cache IO

--- GeoTopicsTests/src/ca/ualberta/cs/team5geotopics/test/CacheTests.java
+++ GeoTopicsTests/src/ca/ualberta/cs/team5geotopics/test/CacheTests.java
@@ -44,7 +44,7 @@ public class CacheTests extends
 		Bitmap pic = Bitmap.createBitmap(10,10 ,Bitmap.Config.ARGB_8888);
 
 		ArrayList<CommentModel> acm = new ArrayList<CommentModel>();
-		CommentModel comment1 = new CommentModel("1.2", "1.111", "Body", "Author", null, "Title");
+		CommentModel comment1 = new CommentModel("1.2", "1.111", "Body-test body", "Author", null, "Title");
 		CommentModel comment2 = new CommentModel("2", "2", "Body2", "Author2", pic, "Title2");
 		
  		acm.add(comment1);
@@ -60,10 +60,24 @@ public class CacheTests extends
 		acm.clear();
 		
 		acm = cache.loadFromCache(filename, activity);
-		assertTrue(!acm.isEmpty()); //so there is something here. What else can it be (how can I view it's contents)
-		assertTrue(acm.contains(comment1));
-		assertTrue(acm.contains(comment2));
+		assertTrue(!acm.isEmpty()); 
 		
+		assertTrue(acm.get(0).getmBody().equals(comment1.getmBody()));
+		assertTrue(acm.get(0).getmAuthor().equals(comment1.getmAuthor()));
+		assertTrue(acm.get(0).getmTitle().equals(comment1.getmTitle()));
+		assertTrue(acm.get(0).getmPicture() == null);
+		assertTrue(acm.get(0).getLat().equals(comment1.getLat()));
+		assertTrue(acm.get(0).getLon().equals(comment1.getLon()));
+		
+		assertTrue(acm.get(1).getmBody().equals(comment2.getmBody()));
+		assertTrue(acm.get(1).getmAuthor().equals(comment2.getmAuthor()));
+		assertTrue(acm.get(1).getmTitle().equals(comment2.getmTitle()));
+		assertTrue(acm.get(1).getLat().equals(comment2.getLat()));
+		assertTrue(acm.get(1).getLon().equals(comment2.getLon()));
+		
+		//Log.w("filterlogstest", acm.get(1).getmPicture().toString());
+		//Log.w("filterlogstest", comment2.getmPicture().toString()); //references change but the picture still makes it.
+		assertTrue(acm.get(1).getmPicture().describeContents() == comment2.getmPicture().describeContents());
 		
 		
 	}
