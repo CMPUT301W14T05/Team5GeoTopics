3cee599f051441a5ac10f0ae911c2b86b19da27a
brycecartman <cartman@ualberta.ca>
2014-03-24 01:10:59 -0600

    Added lng/lat coords on click

--- GeoTopicsActivity/src/ca/ualberta/cs/team5geotopics/MapsActivity.java
+++ GeoTopicsActivity/src/ca/ualberta/cs/team5geotopics/MapsActivity.java
@@ -1,18 +1,28 @@
 package ca.ualberta.cs.team5geotopics;
 
+import java.text.DecimalFormat;
+
 import com.example.team5geotopics.R;
 import com.example.team5geotopics.R.id;
 import com.example.team5geotopics.R.layout;
 import com.example.team5geotopics.R.menu;
-
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.widget.Toast;
 
 
@@ -25,7 +35,6 @@ public class MapsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_maps);
- 
         try {
             // Loading map
             initilizeMap();
@@ -43,6 +52,8 @@ public class MapsActivity extends Activity {
         if (googleMap == null) {
             googleMap = ((MapFragment) getFragmentManager().findFragmentById(
                     R.id.map)).getMap();
+            googleMap.setMyLocationEnabled(true);
+         
  
             // check if map is created successfully or not
             if (googleMap == null) {
@@ -50,13 +61,35 @@ public class MapsActivity extends Activity {
                         "Sorry! unable to create maps", Toast.LENGTH_SHORT)
                         .show();
             }
+            
+            googleMap.setOnMapClickListener(new OnMapClickListener(){
+            
+            @Override
+            public void onMapClick(LatLng point) {
+                Log.d("Map","Map clicked");
+                DecimalFormat form = new DecimalFormat("0.00000");
+                Toast.makeText(getBaseContext(), 
+                		form.format(point.latitude)  + "," + 
+                		form.format(point.longitude)  , 
+                        Toast.LENGTH_SHORT).show();
+                googleMap.addMarker(new MarkerOptions().position(point).title("Clicked!"));
+            	}
+            });
+             
+            
         }
+	        
     }
  
+    
+    
     @Override
     protected void onResume() {
         super.onResume();
         initilizeMap();
     }
  
+    
 }
+
+
