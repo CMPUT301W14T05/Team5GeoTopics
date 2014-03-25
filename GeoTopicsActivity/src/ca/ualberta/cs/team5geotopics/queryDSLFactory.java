cf1e11f7becbeac43ad843f35a8e6caaab23f216
slmyers <slmyers@ualberta.ca>
2014-03-25 14:25:26 -0600

    more work on queryDSLFactory

--- GeoTopicsActivity/src/ca/ualberta/cs/team5geotopics/queryDSLFactory.java
+++ GeoTopicsActivity/src/ca/ualberta/cs/team5geotopics/queryDSLFactory.java
@@ -3,10 +3,15 @@ package ca.ualberta.cs.team5geotopics;
 public class queryDSLFactory {
 	private String sortType;
 	private String filterType;
-	
+	private String filterSpecifier;
+	private int distance;
 	
 	public queryDSLFactory() {
 		
+	}
+	
+	public void setDistance(int distance) {
+		this.distance = distance;
 	}
 
 	public void setSort(String sort) {
@@ -16,41 +21,77 @@ public class queryDSLFactory {
 	public void setFilter(String filter) {
 		this.filterType = filter;
 	}
+	
+	public void setFilterSpecifier(String filterSpecifier){
+		this.filterSpecifier = filterSpecifier;
+	}
 
 	public String createMatchAll(){
-		return "{\n" +
-	  			"\"query\": {\n" +
-	  			"\"match_all\": {}\n" +
-	  			"}\n" +
-	  			"}";
+		if(filterType != null){
+			return "{\n" +
+					"\"filtered\"\n" +
+		  			"\"query\": {\n" +
+		  			"\"match_all\": {}\n" +
+		  			"}\n" +
+		  			"},\n" + createFilterDSL();
+		}
+		if(filterType == null){
+			return "{\n" +
+		  			"\"query\": {\n" +
+		  			"\"match_all\": {}\n" +
+		  			"}\n" +
+		  			"}";
+		}
+		return "ERROR";
 	}
 
 	public String createSortDSL(){
 		if(sortType.equals("time")){
 			return 	"\"sort\" : [\n" +
 					"{\"epochTime\" : {\"order\" : \"asc\", \"mode\" : \"avg\"}}\n" +
-					"],\n";
+					"]\n" +
+					"}";
 		}
 		return "ERROR";
 	}
 	
-	public String createFilterDSL(String specifier){
+	public String createFilterDSL(){
+		if(filterSpecifier == null){
+			return "Filter not specified Error";
+		}
 		if(filterType.equals("location")){
-			return "\"filter\" : {\n" +
+			if(sortType == null){
+				return "\"filter\" : {\n" +
+						"\"geo_distance\" : {\n" +
+						"\"distance\" : \"" + Integer.valueOf(this.distance).toString() + "km\",\n" +
+						"\"pin.location\" : \"" + filterSpecifier + "\"\n" +
+						"}\n" +
+						"}\n";
+			}
+			return 	"\"filter\" : {\n" +
 					"\"geo_distance\" : {\n" +
-					"\"distance\" : \"12km\",\n" +
-					"\"pin.location\" : \"" + specifier + "\"\n" +
+					"\"distance\" : \"" + Integer.valueOf(this.distance).toString() + "km\",\n" +
+					"\"pin.location\" : \"" + filterSpecifier + "\"\n" +
 					"}\n" +
-					"}\n";
+					"},\n" + createSortDSL();
 		}
 		if(filterType.equals("replies")){
-			return  "{\n" + 					
-					"\"filter\": {\n" + 
-					"\"type\":{\n" + 
-					"\"value\" : \"" + specifier +  "\"\n"  + 
-					"}\n" +
-					"}\n" +
-					"}";
+			if(sortType == null){
+				return  "{\n" + 					
+						"\"filter\": {\n" + 
+						"\"type\":{\n" + 
+						"\"value\" : \"" + filterSpecifier +  "\"\n"  + 
+						"}\n" +
+						"}\n" +
+						"}";
+			}
+			return 		"{\n" + 					
+						"\"filter\": {\n" + 
+						"\"type\":{\n" + 
+						"\"value\" : \"" + filterSpecifier +  "\"\n"  + 
+						"}\n" +
+						"}\n" +
+						"},\n" + createSortDSL();
 		}
 		
 		return "ERROR";
