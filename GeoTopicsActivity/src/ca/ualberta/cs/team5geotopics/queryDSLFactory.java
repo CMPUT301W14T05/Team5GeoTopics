9e9a46a0c60afd60c168bcdca99659642ac282de
slmyers <slmyers@ualberta.ca>
2014-03-25 13:54:57 -0600

    updating dsl factory

--- GeoTopicsActivity/src/ca/ualberta/cs/team5geotopics/queryDSLFactory.java
+++ GeoTopicsActivity/src/ca/ualberta/cs/team5geotopics/queryDSLFactory.java
@@ -2,34 +2,59 @@ package ca.ualberta.cs.team5geotopics;
 
 public class queryDSLFactory {
 	private String sortType;
+	private String filterType;
 	
 	
-	
-	public queryDSLFactory(String sort) {
-		this.sortType = sort;
+	public queryDSLFactory() {
+		
 	}
-
-
-
-
-
 
 	public void setSort(String sort) {
 		this.sortType = sort;
 	}
+	
+	public void setFilter(String filter) {
+		this.filterType = filter;
+	}
 
+	public String createMatchAll(){
+		return "{\n" +
+	  			"\"query\": {\n" +
+	  			"\"match_all\": {}\n" +
+	  			"}\n" +
+	  			"}";
+	}
 
-
-	public String createDSL(){
+	public String createSortDSL(){
 		if(sortType.equals("time")){
 			return 	"\"sort\" : [\n" +
 					"{\"epochTime\" : {\"order\" : \"asc\", \"mode\" : \"avg\"}}\n" +
 					"],\n";
 		}
-		
-		
-		return null;
+		return "ERROR";
 	}
 	
+	public String createFilterDSL(String specifier){
+		if(filterType.equals("location")){
+			return "\"filter\" : {\n" +
+					"\"geo_distance\" : {\n" +
+					"\"distance\" : \"12km\",\n" +
+					"\"pin.location\" : \"" + specifier + "\"\n" +
+					"}\n" +
+					"}\n";
+		}
+		if(filterType.equals("replies")){
+			return  "{\n" + 					
+					"\"filter\": {\n" + 
+					"\"type\":{\n" + 
+					"\"value\" : \"" + specifier +  "\"\n"  + 
+					"}\n" +
+					"}\n" +
+					"}";
+		}
+		
+		return "ERROR";
+	}
 	
+
 }
