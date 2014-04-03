28c69d80a70a4cfbeb5750de8d10fa9351851822
twendlandt <twendlan@ualberta.ca>
2014-04-03 13:55:04 -0600

    working on help page

---
+++ GeoTopicsActivity/src/ca/ualberta/cs/team5geotopics/HelpActivity.java
@@ -0,0 +1,16 @@
+package ca.ualberta.cs.team5geotopics;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
